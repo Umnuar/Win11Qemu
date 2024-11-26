34b91c896685cef34e3ff1093c08d989f81a0635
Umnuar <155422868+Umnuar@users.noreply.github.com>
2024-11-26 08:47:30 +0700

    Update a.py

--- a.py
+++ a.py
@@ -5,7 +5,7 @@ file_id = "1GTgc6v8EwKYFQRjAy8FXghXYPKBDZWGJ"
 # URL tải file
 url = f"https://drive.google.com/uc?id={file_id}"
 # Tên file khi tải về
-output = "/mnt/win11.zip"
+output = "/home/win11.zip"
 
 # Tải file
 gdown.download(url, output, quiet=False)
